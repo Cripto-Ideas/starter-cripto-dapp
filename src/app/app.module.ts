e6375378e44d39742964b3bb30a000bad81c80c6
Tecla Roig <ch.roig@gmail.com>
2022-02-05 15:01:08 -0300

    Crear Componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppBoxComponent } from './shared/app-box/app-box.component';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LinksComponent } from './pages/links/links.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppBoxComponent,
+    NotFoundComponent,
+    HomeComponent,
+    LinksComponent
   ],
   imports: [
     BrowserModule,
