b90c9de3c688152f729a210e630a289f1a253992
Christian Roig <ch.roig@gmail.com>
2022-02-05 23:22:45 -0300

    codificar servicios web3 - ERROR

--- src/app/core/web3.ts
+++ src/app/core/web3.ts
@@ -1,2 +1,7 @@
-export class Web3 {
-}
+import { InjectionToken } from '@angular/core';
+import Web3 from 'web3';
+
+export const WEB3 = new InjectionToken<Web3>('web3', {
+  providedIn: 'root',
+  factory: () => new Web3(Web3.givenProvider)
+});
