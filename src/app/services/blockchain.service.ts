3c171f1bf9895e4db916c01e4103470a69bae66f
Christian Roig <ch.roig@gmail.com>
2022-02-06 23:16:23 -0300

    add web code

--- src/app/services/blockchain.service.ts
+++ src/app/services/blockchain.service.ts
@@ -1,13 +1,38 @@
 import { Injectable } from '@angular/core';
+import Web3 from 'web3';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlockchainService {
 
-  constructor() {
+
+  private windowBrowser: any;
+
+  constructor() { 
     
     console.log('BlockChain Service Construct');
+    this.windowBrowser = window;
+  }
 
-   }
+  async loadWeb3() {
+    
+    console.log(this.windowBrowser.ethereum);
+
+    if(this.windowBrowser.ethereum) {
+      console.log('es eth');
+      this.windowBrowser.web3 = new Web3(this.windowBrowser.ethereum)
+      await this.windowBrowser.ethereum.enable()
+    }
+
+    else if (this.windowBrowser.web3) {
+      this.windowBrowser.web3 = new Web3(this.windowBrowser.web3.currentProvider)
+    }
+
+    else {
+      window.alert('¡Considera usar Metamask!')
+    }
+
+    console.log(this.windowBrowser.web3);
+  }
 }
